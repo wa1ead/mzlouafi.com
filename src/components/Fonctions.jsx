64d205fa76e4e9966c143ab11856301c390587ea
wa1ead <waliddraa2@gmail.com>
2023-09-06 14:18:35 +0100

    add services

--- src/components/Fonctions.jsx
+++ src/components/Fonctions.jsx
@@ -1,7 +1,12 @@
-export default function Fonctions() {
+import WebDev from "../assets/web_dev.jpg";
+import MobDev from "../assets/mobile_dev.jpg";
+import MarkDig from "../assets/marketing_dig.jpg";
+import GraphDes from "../assets/graphic_design.jpg";
+
+export default function Services() {
   return (
     // <!-- Card Blog -->
-    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    <div className="max-w-[85rem] bg-blue-950 rounded-lg px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-8">
       {/* <!-- Grid --> */}
       <div className="grid lg:grid-cols-2 lg:gap-y-16 gap-10">
         {/* <!-- Card --> */}
@@ -10,35 +15,21 @@ export default function Fonctions() {
             <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
               <img
                 className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 left-0 object-cover rounded-xl"
-                src="https://images.unsplash.com/photo-1540575861501-7cf05a4b125a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                src={WebDev}
                 alt="Image Description"
               />
             </div>
 
             <div className="grow mt-4 sm:mt-0 sm:ml-6 px-4 sm:px-0">
               <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
-                Studio by Preline
+                DEVELOPPEMENT WEB
               </h3>
               <p className="mt-3 text-gray-600 dark:text-gray-400">
-                Produce professional, reliable streams easily leveraging
-                Preline's innovative broadcast studio
+                Créez une présence en ligne exceptionnelle avec notre service de
+                développement web.
               </p>
               <p className="mt-4 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 hover:underline font-medium">
                 Read more
-                <svg
-                  className="w-2.5 h-2.5"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                >
-                  <path
-                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  />
-                </svg>
               </p>
             </div>
           </div>
@@ -51,35 +42,21 @@ export default function Fonctions() {
             <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
               <img
                 className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 left-0 object-cover rounded-xl"
-                src="https://images.unsplash.com/photo-1668906093328-99601a1aa584?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80"
+                src={MobDev}
                 alt="Image Description"
               />
             </div>
 
             <div className="grow mt-4 sm:mt-0 sm:ml-6 px-4 sm:px-0">
               <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
-                Onsite
+                DEVELOPPEMENT MOBILE
               </h3>
               <p className="mt-3 text-gray-600 dark:text-gray-400">
-                Optimize your in-person experience with best-in-className
-                capabilities like badge printing and lead retrieval
+                Transformez vos idées en applications mobiles captivantes grâce
+                à notre service de développement mobile.
               </p>
               <p className="mt-4 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 hover:underline font-medium">
                 Read more
-                <svg
-                  className="w-2.5 h-2.5"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                >
-                  <path
-                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  />
-                </svg>
               </p>
             </div>
           </div>
@@ -92,34 +69,21 @@ export default function Fonctions() {
             <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
               <img
                 className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 left-0 object-cover rounded-xl"
-                src="https://images.unsplash.com/photo-1567016526105-22da7c13161a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                src={MarkDig}
                 alt="Image Description"
               />
             </div>
 
             <div className="grow mt-4 sm:mt-0 sm:ml-6 px-4 sm:px-0">
               <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
-                The complete guide to OKRs
+                MARKETING DIGITAL
               </h3>
               <p className="mt-3 text-gray-600 dark:text-gray-400">
-                How to make objectives and key results work for your company
+                Maximisez votre visibilité en ligne avec notre service de
+                marketing digital.
               </p>
               <p className="mt-4 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 hover:underline font-medium">
                 Read more
-                <svg
-                  className="w-2.5 h-2.5"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                >
-                  <path
-                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  />
-                </svg>
               </p>
             </div>
           </div>
@@ -132,34 +96,21 @@ export default function Fonctions() {
             <div className="flex-shrink-0 relative rounded-xl overflow-hidden w-full sm:w-56 h-44">
               <img
                 className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full absolute top-0 left-0 object-cover rounded-xl"
-                src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                src={GraphDes}
                 alt="Image Description"
               />
             </div>
 
             <div className="grow mt-4 sm:mt-0 sm:ml-6 px-4 sm:px-0">
               <h3 className="text-xl font-semibold text-gray-800 group-hover:text-gray-600 dark:text-gray-300 dark:group-hover:text-white">
-                People program models
+                DESIGN GRAPHIQUE
               </h3>
               <p className="mt-3 text-gray-600 dark:text-gray-400">
-                Six approaches to bringing your People strategy to life
+                Exprimez votre identité visuelle avec notre service de design
+                graphique.
               </p>
               <p className="mt-4 inline-flex items-center gap-x-1.5 text-blue-600 decoration-2 hover:underline font-medium">
                 Read more
-                <svg
-                  className="w-2.5 h-2.5"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                >
-                  <path
-                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  />
-                </svg>
               </p>
             </div>
           </div>
