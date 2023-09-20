b178c8dcf4a2326b5b15171fe602126220056859
wa1ead <waliddraa2@gmail.com>
2023-09-20 12:57:59 +0100

    editing about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,18 @@
-import mark from "../assets/mark.png";
+import about from "../assets/about.jpg";
 
 export default function Approche() {
   return (
-    <div className="py-20 my-20">
-      <h2 className="text-4xl font-mons text-center text-blue-950 pb-4 lg:text-6xl">
-        Notre Approche
-      </h2>
-      <div className="mx-auto max-w-7xl lg:px-8 rounded-md bg-slate-950">
-        <div className="text-lg font-inter text-white text-center pt-12 leading-8 lg:text-xl">
-          <p>
+    <div>
+      <div className="w-full h-[700px] bg-gray-900/90 absolute">
+        <img
+          className="w-full h-full object-cover mix-blend-overlay"
+          src={about}
+        />
+      </div>
+      <div className="max-w-[1240px] mx-auto text-white relative ">
+        <div className="px-4 py-12">
+          <h2 className="head_text pt-20">À propos de nous:</h2>
+          <p className="desc py-4">
             MZ LOUAFI SARL A.U est une agence de communication digitale crée le
             26 août 2021, spécialisée en marketing digital, Création de site
             internet, Application WEB & Mobile, Référencement (SEO), Design
@@ -19,69 +23,59 @@ export default function Approche() {
             sophistiquée et des expériences numériques percutantes.
           </p>
         </div>
-        <div className="mx-auto my-4 flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
-          <div
-            className="w-full mt-20 lg:max-w-xl lg:flex-auto"
-            data-aos="fade-right"
-            data-aos-duration="800"
-          >
-            <h3 className="sr-only">Job openings</h3>
-            <ul className="-my-8 divide-y divide-white">
-              {approcheDetails.map((approche) => (
-                <li key={approche.id} className="py-8">
-                  <dl className="relative flex flex-wrap gap-x-3">
-                    <dt className="sr-only">Role</dt>
-                    <dd className="w-full flex-none text-lg font-mons tracking-tight text-blue-400">
-                      {approche.title}
-                      <span className="absolute inset-0" aria-hidden="true" />
-                    </dd>
-                    <dt className="sr-only">Description</dt>
-                    <dd className="mt-2 w-full flex-none font-inter text-base leading-7 text-white">
-                      {approche.description}
-                    </dd>
-                  </dl>
-                </li>
-              ))}
-            </ul>
+        <div className="grid grid-cols-1 lg:grid-cols-3 relative gap-x-4 gap-y-8 px-4 pt-10 mb-10 text-black">
+          <div className="bg-white rounded-xl shadow-2xl">
+            <div className="p-4">
+              <h3 className="text-2xl text-gray-600 my-6">
+                Inspirer et captiver :{" "}
+              </h3>
+              <p className="text-lg font-bold ">
+                La création est le point de départ de toute communication
+                digitale efficace. Notre équipe de créatifs talentueux excelle
+                dans la conception de contenu visuel et écrit percutant. Que ce
+                soit à travers des vidéos accrocheuses, des infographies
+                informatives, des articles de blog engageants, ou même des
+                publicités innovantes, nous nous efforçons de créer un contenu
+                qui se démarque dans le bruit numérique.
+              </p>
+            </div>
           </div>
-          <div className="w-full lg:max-w-lg lg:flex-auto">
-            <img
-              src={mark}
-              alt="keep it simple"
-              className=" mb-24 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]"
-            />
+          <div className="bg-white rounded-xl shadow-2xl">
+            <div className="p-4">
+              <h3 className="text-2xl text-gray-600 my-6">
+                Établir des liens significatifs :{" "}
+              </h3>
+              <p className="text-lg font-bold ">
+                La véritable puissance de la communication digitale réside dans
+                sa capacité à établir des connexions significatives avec votre
+                public cible. Nous utilisons des stratégies de médias sociaux,
+                de référencement, de marketing par e-mail et bien d'autres
+                tactiques pour garantir que votre message atteigne les bonnes
+                personnes au bon moment. Nous croyons en la construction de
+                relations durables avec votre audience pour créer une fidélité à
+                long terme.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="m-8 justify-center flex border-t border-white pt-8">
-          <a
-            href="/contact"
-            className="text-lg font-semibold mb-5 leading-6 text-blue-100 hover:text-blue-500"
-          >
-            Contactez-nous<span aria-hidden="true">&rarr;</span>
-          </a>
+          <div className="bg-white rounded-xl shadow-2xl">
+            <div className="p-4">
+              <h3 className="text-2xl text-gray-600 my-6">
+                Transformer les prospects en clients :{" "}
+              </h3>
+              <p className="text-lg font-bold ">
+                Une communication digitale efficace ne se limite pas à
+                l'engagement ; elle doit également conduire à des résultats
+                concrets. Notre approche est axée sur la conversion. Nous
+                travaillons à optimiser votre site web pour encourager les
+                actions telles que les achats en ligne, les demandes de devis,
+                les inscriptions à la newsletter, etc. En utilisant des
+                techniques avancées d'analyse et d'optimisation, nous maximisons
+                le retour sur investissement de vos campagnes.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-const approcheDetails = [
-  {
-    id: 1,
-    title: "Inspirer et captiver",
-    description:
-      "La création est le point de départ de toute communication digitale efficace. Notre équipe de créatifs talentueux excelle dans la conception de contenu visuel et écrit percutant. Que ce soit à travers des vidéos accrocheuses, des infographies informatives, des articles de blog engageants, ou même des publicités innovantes, nous nous efforçons de créer un contenu qui se démarque dans le bruit numérique.",
-  },
-  {
-    id: 2,
-    title: " Établir des liens significatifs",
-    description:
-      "La véritable puissance de la communication digitale réside dans sa capacité à établir des connexions significatives avec votre public cible. Nous utilisons des stratégies de médias sociaux, de référencement, de marketing par e-mail et bien d'autres tactiques pour garantir que votre message atteigne les bonnes personnes au bon moment. Nous croyons en la construction de relations durables avec votre audience pour créer une fidélité à long terme.",
-  },
-  {
-    id: 3,
-    title: "Transformer les prospects en clients",
-    description:
-      "Une communication digitale efficace ne se limite pas à l'engagement ; elle doit également conduire à des résultats concrets. Notre approche est axée sur la conversion. Nous travaillons à optimiser votre site web pour encourager les actions telles que les achats en ligne, les demandes de devis, les inscriptions à la newsletter, etc. En utilisant des techniques avancées d'analyse et d'optimisation, nous maximisons le retour sur investissement de vos campagnes.",
-  },
-];
